e85f3535a662310f039567efc7ee4193cdf9cede
kengz <kengzwl@gmail.com>
2016-10-08 15:08:44 -0400

    add polyglot ioserver with own logger

---
+++ src/start-io.js
@@ -0,0 +1,115 @@
+// The socket.io server and polyglot clients. Called by scripts/_init.js
+const Promise = require('bluebird')
+const { spawn, execSync } = require('child_process')
+const express = require('express')
+const fs = require('fs')
+const http = require('http')
+const _ = require('lodash')
+const path = require('path')
+const socketIO = require('socket.io')
+const winston = require('winston')
+const app = express()
+const server = http.Server(app)
+var log = winston // for logging here
+
+/**
+ * Start a Socket IO server connecting to a brand new Express server for use in dev. Sets global.io too.
+ * Has a failsafe for not starting multiple times even when called repeatedly by accident in the same thread.
+ * @param  {*} robot The hubot object
+ * @return {server} server
+ */
+ /* istanbul ignore next */
+ function ioServer(port=6466, clientCount=1, timeoutMs=15000) {
+  if (global.io) {
+    // if already started
+    return Promise.resolve()
+  }
+
+  log.info(`Starting poly-socketio server on port: ${port}, expecting ${clientCount} IO clients`)
+  global.io = socketIO(server)
+  var count = clientCount
+  global.ioPromise = new Promise((resolve, reject) => {
+    global.io.sockets.on('connection', (socket) => {
+      // serialize for direct communication by using join room
+      socket.on('join', (id) => {
+        socket.join(id)
+        count--
+        log.debug(`${id} ${socket.id} joined, ${count} remains`)
+        if (count <= 0) {
+          log.info(`All ${clientCount} IO clients have joined`)
+          resolve(server) // resolve with the server
+        }
+      })
+      socket.on('disconnect', () => { log.info(socket.id, 'left') })
+    })
+  })
+  .timeout(timeoutMs)
+  .catch((err) => {
+    log.error(JSON.stringify(err, null, 2))
+    log.error("Expected number of IO clients failed to join in time")
+    process.exit(1)
+  })
+
+  global.io.on('connection', (socket) => {
+    // generic pass to other script
+    socket.on('pass', (msg, fn) => {
+      log.debug(`IO on pass, msg: ${JSON.stringify(msg, null, 2)} fn: ${fn}`)
+      try {
+        // e.g. split 'hello.py' into ['hello', 'py']
+        // lang = 'py', module = 'hello'
+        var tokens = msg.to.split('.'),
+        lang = tokens.pop(),
+        module = _.join(tokens, '.')
+          // reset of <to> for easy calling. May be empty if just passing to client.<lang>
+          msg.to = module
+          global.io.sockets.in(lang).emit('take', msg)
+        } catch (e) { log.error(JSON.stringify(e, null, 2)) }
+      })
+  })
+
+  return new Promise((resolve, reject) => {
+    server.listen(port, () => {
+      resolve()
+    })
+  })
+}
+
+function initLogger(debug) {
+  logLevel = debug ? 'debug' : 'info'
+  log = new(winston.Logger)({
+    level: logLevel,
+    transports: [new(winston.transports.Console)({
+      formatter: (options) => {
+        /* istanbul ignore next */
+        return `[${new Date}] ${winston.config.colorize(options.level, options.level.toUpperCase())} ${options.message || ''}`
+      }
+    })]
+  })
+}
+
+/**
+ * The main method to start the io server.
+ * options = {port, clientCount, timeoutMs, debug}
+ */
+ /* istanbul ignore next */
+ function ioStart(options) {
+  options = options || {}
+  initLogger(options.debug)
+  ioServer(options.port, options.clientCount, options.timeoutMs)
+  return global.ioPromise
+}
+
+/* istanbul ignore next */
+const cleanExit = () => { process.exit() }
+process.on('SIGINT', cleanExit) // catch ctrl-c
+process.on('SIGTERM', cleanExit) // catch kill
+process.on('uncaughtException', log.error)
+
+// export for use by hubot
+module.exports = ioStart
+
+// if this file is run directly by `node server.js`
+/* istanbul ignore next */
+if (require.main === module) {
+  ioStart()
+}
